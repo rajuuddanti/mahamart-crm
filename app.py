c1bf7f4b47b1cd8c14653e0e3e985a038003f3f4
Raju <itteam@mahamartco.in>
2026-08-31 16:46:14 +0530

    Hide streamlit menus and buttons

--- app.py
+++ app.py
@@ -5,6 +5,19 @@ from datetime import datetime, timedelta
 
 # --- PAGE SETUP ---
 st.set_page_config(page_title="MahaMart CRM", layout="wide")
+
+# --- HIDE STREAMLIT BRANDING AND MENUS ---
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            header {visibility: hidden;}
+            footer {visibility: hidden;}
+            .stApp [data-testid="stToolbar"] {display: none;}
+            .viewerBadge_container__1QSob {display: none;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 st.title("📊 MahaMart CRM & Performance Dashboard")
 
 # --- INDIAN CURRENCY FORMATTER FUNCTION ---
